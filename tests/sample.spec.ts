3324f7934d1f265bb41759af798e0e9236f28d16
Hiroaki Date <mmst3l4@gmail.com>
2022-05-28 21:04:16 +0900

    vitest導入のためのオプションを追記し、サンプルとなるテストを追加

---
+++ tests/sample.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+
+describe('calculate', () => {
+  it('1 + 1 = 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it('10 / 4', () => {
+    expect(10 / 4).toEqual(2.5);
+  });
+});
